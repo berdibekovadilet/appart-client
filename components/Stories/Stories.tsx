846385eebeacc0534e8f0f2180b1bdf9ad943e68
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-08-18 18:22:53 +0600

    added stories carousel

--- components/Stories/Stories.tsx
+++ components/Stories/Stories.tsx
@@ -2,19 +2,42 @@ import Image from "next/image";
 import React from "react";
 import { storiesCard } from "../../data";
 import styles from "./Stories.module.scss";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
 
 const Stories = () => {
+  const [width, setWidth] = useState(0);
+  //   const carousel = useRef();
+  const carousel = React.createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    console.log(
+      carousel.current?.scrollWidth! - carousel.current?.offsetWidth!
+    );
+    setWidth(carousel.current?.scrollWidth! - carousel.current?.offsetWidth!);
+  }, [carousel]);
+
   return (
-    <div className={styles.container}>
-      {storiesCard.map((item) => (
-        <div className={styles.card} key={item.id}>
-          <div className={styles.imgContainer}>
-            <Image src={item.image} alt="stories" width="104" height="104" />
-          </div>
-          <p className={styles.description}>{item.description}</p>
-        </div>
-      ))}
-    </div>
+    <motion.div
+      className={styles.carousel}
+      ref={carousel}
+      whileTap={{ cursor: "grabbing" }}
+    >
+      <motion.div
+        className={styles.innerCarousel}
+        drag="x"
+        dragConstraints={{ right: 0, left: -60 }}
+      >
+        {storiesCard.map((item) => (
+          <motion.div className={styles.item} key={item.id}>
+            <div className={styles.imgContainer}>
+              <Image src={item.image} alt="stories" width="102" height="102" />
+            </div>
+            <p className={styles.description}>{item.description}</p>
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
