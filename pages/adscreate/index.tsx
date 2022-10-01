ba7557bb6fd5fab747ac4044dc15b7573cab49e4
adilet berdibekov <berdibekovadilet@gmail.com>
2022-10-01 15:56:15 +0600

    added yandex map

--- pages/adscreate/index.tsx
+++ pages/adscreate/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Head from "next/head";
 
+import { Button, Form, Input } from "antd";
+import { YMaps, Map, Clusterer, Placemark } from "react-yandex-maps";
+
 import PageHeader from "../../components/PageHeader/PageHeader";
 import Title from "../../components/Title/Title";
 import IconCard from "../../components/IconCard/IconCard";
@@ -9,6 +12,13 @@ import { adType, propertyType } from "../../data";
 import styles from "./Adscreate.module.scss";
 
 const Objectcreate = () => {
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <>
       <Head>
@@ -40,9 +50,45 @@ const Objectcreate = () => {
           </div>
         </div>
         <div className={styles.row}>
-          <Title size="h3">Адрес</Title>
-          <div className={styles.cardWrapper}></div>
+          <div className={styles.adressTitle}>
+            <Title size="h3">Адрес:</Title>
+            <button>Бишкек</button>
+          </div>
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="on"
+          >
+            <Form.Item
+              name="Адрес или название ЖК"
+              rules={[
+                {
+                  required: true,
+                  message: "Укажите адрес или название ЖК",
+                },
+              ]}
+            >
+              <Input
+                placeholder="Укажите адрес или название ЖК"
+                size="large"
+                type="adress"
+              />
+            </Form.Item>
+          </Form>
         </div>
+        <YMaps query={{ apikey: "26627679-7264-4409-98a4-22eb522d227d" }}>
+          <div className={styles.mapWrapper}>
+            <Map
+              width="100%"
+              height="300px"
+              defaultState={{ center: [42.87, 74.6], zoom: 12 }}
+            />
+          </div>
+        </YMaps>
       </div>
     </>
   );
